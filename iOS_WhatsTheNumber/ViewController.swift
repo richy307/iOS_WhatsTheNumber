750a2c90aea5163c8ea6ddd92ed2f4e0334fe4e3
Richy <richy307@gmail.com>
2023-07-11 10:39:14 +0800

    取亂數, becomeFirstResponder

--- iOS_WhatsTheNumber/ViewController.swift
+++ iOS_WhatsTheNumber/ViewController.swift
@@ -8,17 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // make a random number
+    var answer = Int.random(in: 1...100)
 
     @IBOutlet weak var messageLabel: UILabel!
     
     @IBOutlet weak var inputTextField: UITextField!
     
     @IBAction func makeGuess(_ sender: UIButton) {
+        print(answer)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // push the keyboard up // 觸發輸入匡讓鍵盤出現
+        inputTextField.becomeFirstResponder()
     }
 
 
